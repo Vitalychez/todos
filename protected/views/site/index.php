260b5efebf3c387ffc06cb228e94a4a3a3851389
Vitaliy Lukyanov <Vitalychez1@gmail.com>
2018-02-18 18:54:35 +0700

    Init

---
+++ protected/views/site/index.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'What to do';
+?>
+
+<div class="wrap">
+    <div class="container">
+        <div class="jumbotron">
+            <p><a class="btn btn-lg btn-info" href="/registration">Регистрация</a></p>
+            <p><a class="btn btn-lg btn-success" href="/login">Вход</a></p>
+        </div>
+    </div>
+</div>
+
